7dbce4bb06f19d0be555a7871596a38621f308f7
Giovanni Aguirre <g3aguirr@ucsd.edu>
2018-04-24 17:44:12 -0700

    stuff

--- petsapp-v1-skeleton/server.js
+++ petsapp-v1-skeleton/server.js
@@ -1,1 +1,38 @@
-// Node.js + Express backend ...
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express();
+app.use(express.static('static_files'));
+app.use(express.bodyParser());
+
+const songDatabase = {
+  'fakeSong': {original: 'a', tranlated: 'ah'},
+  'fakeSong2': {original: 'b', translated: 'bee'}
+};
+
+
+
+
+
+app.get('/songs', (req, res) => {
+
+  const allSongtitles = Object.keys(songDatabase);
+  //console.log('songs are:', songDatabase);
+  res.send(allSongtitles);
+});
+
+app.get('/songs/:songname', (req, res) => {
+  const nameToLookup = req.params.songname;
+
+  const allSongtitles = Object.keys(songDatabase);
+  res.send(allSongtitles);
+})
+
+app.post('/songs/:songname', (req, res) => {
+  const name = req.params.songname;
+  songDatabase[name] = {original: req.body};
+  //song.original = req.body;
+  console.log(req.body);
+})
+app.listen(3000, () => {
+  console.log('server started at http://localhost:3000/');
+});
