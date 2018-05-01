01de1d78a3db1b3199d37dccedade79718ff51b9
Giovanni Aguirre <g3aguirr@ucsd.edu>
2018-04-30 21:01:19 -0700

    added sqlite db

---
+++ LyricApp/create_db.js
@@ -0,0 +1,26 @@
+
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('lyrics.db');
+
+// run each database statement *serially* one after another
+// (if you don't do this, then all statements will run in parallel,
+//  which we don't want)
+db.serialize(() => {
+  // create a new database table:
+  db.run("CREATE TABLE songs_to_lyrics (song TEXT, original TEXT, translated TEXT)");
+
+  // insert 3 rows of data:
+  db.run("INSERT INTO songs_to_lyrics VALUES ('fakesong', 'fakelyric', 'fakelyrict')");
+  db.run("INSERT INTO songs_to_lyrics VALUES ('fakeSong2', 'fakelyric2', 'fakelyric2t')");
+
+
+  console.log('successfully created the songs_to_lyrics table in lyrics.db');
+
+  // print them out to confirm their contents:
+  db.each("SELECT song, original, translated FROM songs_to_lyrics", (err, row) => {
+      console.log(row.song + ": " + row.original + ' - ' + row.translated);
+  });
+});
+
+db.close();
--- LyricApp/server.js
+++ LyricApp/server.js
@@ -1,46 +1,95 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 const app = express();
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('lyrics.db');
+
 app.use(express.static('static_files'));
 app.use(express.bodyParser());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const songDatabase = {
+/*const songDatabase = {
   'fakeSong': {original: 'a', tranlated: 'ah'},
   'fakeSong2': {original: 'b', translated: 'bee'}
-};
+};*/
 
 
 
 
 
 app.get('/songs', (req, res) => {
-
-  const allSongtitles = Object.keys(songDatabase);
+  db.all('SELECT song FROM songs_to_lyrics', (err, rows) => {
+    console.log(rows);
+    const allSongs = rows.map(e => e.song);
+    console.log(allSongs);
+    res.send(allSongs);
+  });
+  //const allSongtitles = Object.keys(songDatabase);
   //console.log('songs are:', songDatabase);
-  res.send(allSongtitles);
+  //res.send(allSongtitles);
 });
 
 app.get('/songs/:songname', (req, res) => {
   const nameToLookup = req.params.songname;
+  db.all(
+    'SELECT * FROM songs_to_lyrics WHERE song=$song',
+    {
+      $song: nameToLookup
+    },
+    (err, rows) => {
+       console.log(rows);
+       if (rows.length > 0) {
+         res.send(rows[0]);
+       } else {
+         res.send({});
+       }
+    }
+  );
 
-  const allSongtitles = Object.keys(songDatabase);
-  res.send(allSongtitles);
 })
 
 app.get('/select/:songname', (req, res) =>{
   const songToLookup = req.params.songname;
-  const song = songDatabase[songToLookup];
-  res.send(song);
+  db.all(
+    'SELECT * FROM songs_to_lyrics WHERE song=$song',
+    {
+      $song: songToLookup
+    },
+    (err, rows) => {
+      if (rows.length > 0) {
+        res.send(rows[0]);
+      } else {
+        res.send({});
+      }
+    }
+  );
+
 })
 app.post('/songs/:songname', (req, res) => {
   const name = req.params.songname;
-  songDatabase[name] = {original: req.body.original, translated: req.body.translated};
+  db.run(
+    'INSERT INTO songs_to_lyrics VALUES ($song, $original, $translated)',
+    {
+      $song: name,
+      $original: req.body.original,
+      $translated: req.body.original
+    },
+    (err) => {
+      if(err) {
+        res.send({message: 'error in app.post(/songs/:songname)'});
+      } else {
+        res.send({message: 'successfully run app.post(/song/:songname)'});
+      }
+    }
+
+  );
+  //songDatabase[name] = {original: req.body.original, translated: req.body.translated};
   //song.original = req.body;
-  console.log(songDatabase[name]);
-})
+  //console.log(songDatabase[name]);
+});
 
 
 
