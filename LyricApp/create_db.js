0d1a3d7ad7e154f009527bda6c359bbbaf76f5fa
William Zhu <wbzhu@ucsd.edu>
2018-05-27 17:39:34 -0700

    logins and create accounts work

--- LyricApp/create_db.js
+++ LyricApp/create_db.js
@@ -8,6 +8,12 @@ const db = new sqlite3.Database(dbname);
 db.serialize(() => {
   // create a new database table:
   db.run("CREATE TABLE songs_to_lyrics (title TEXT, artist TEXT, language TEXT, oLyric TEXT, tLyric TEXT)");
+  
+  // table 'account' does not allow duplicate usernames
+  db.run("CREATE TABLE account (username TEXT, password TEXT NOT NULL, PRIMARY KEY (username))");
+  
+  // table 'score' has a many to one relationship to 'account' username
+  db.run("CREATE TABLE score (username TEXT NOT NULL, score INTEGER NOT NULL, FOREIGN KEY (username) REFERENCES account(username))");
 
   // insert 3 rows of data:
   db.run("INSERT INTO songs_to_lyrics VALUES ('Hirugohan', 'Gundam', 'ja', 'Hirugohan taberu', 'I eat lunch')");
@@ -15,8 +21,13 @@ db.serialize(() => {
   // To check if artist and album works
   db.run("INSERT INTO songs_to_lyrics VALUES ('Hirugohan', 'Totoro', 'JP', 'Chuushoku toru', 'I eat lunch')");
   // Multiline try
-  db.run("INSERT into songs_to_lyrics VALUES('Untouchable, Part 1', 'Anathema', 'sp', 'Tuve que dejarte ir' || char(10) || 'A la puesta de sol', 'I had to let you go' || char(10) || 'To the setting sun')");
+  db.run("INSERT into songs_to_lyrics VALUES ('Untouchable, Part 1', 'Anathema', 'sp', 'Tuve que dejarte ir' || char(10) || 'A la puesta de sol', 'I had to let you go' || char(10) || 'To the setting sun')");
 
+  db.run("INSERT INTO account VALUES ('admin', 'admin')");
+  db.run("INSERT INTO score VALUES ('admin', 50)");
+  db.run("INSERT INTO score VALUES ('admin', 100)");
+  db.run("INSERT INTO score VALUES ('admin', 300)");
+  db.run("INSERT INTO score VALUES ('admin', 40)");
 
   console.log('successfully created the songs_to_lyrics table in ' + dbname);
   console.log('--------------------');
