aabe76a11a2ade0492a9c18ce920f450e5f9e928
William Zhu <wbzhu@ucsd.edu>
2018-06-01 19:50:27 -0700

    added graph for average user scores, now has columns too

--- LyricApp/static_files/js/scores.js
+++ LyricApp/static_files/js/scores.js
@@ -7,14 +7,14 @@ $(() => {
     const layout = {
       title: title,
       xaxis: {
-        title: 'x'
+        title: 'Date'
       },
       yaxis: {
-        title: 'y'
+        title: 'Score'
       }
     };
 
-    Plotly.newPlot("scores-" + id, data, layout);
+    Plotly.newPlot("scores-graph-" + id, data, layout);
   }
 
   const setTrace = (x, y, color, type) => {
@@ -70,7 +70,8 @@ $(() => {
           if (!data.status) {
             $('#searchResult').html(data.message);
           } else if (data.unique) {
-            getScoreAndGraph(title, artist);
+            graphUserScores(title, artist);
+            graphAverageUserScores(title, artist);
           } else {
             $('#searchResult').html('<h3>Search Results:</h3><ul id=song-list></ul>');
             data.songs.forEach((e) => {
@@ -86,10 +87,11 @@ $(() => {
     e.preventDefault();
     const songId = $(event.target).text();
     const splitted = songId.split(' - ');
-    getScoreAndGraph(splitted[1], splitted[0]);
+    graphUserScores(splitted[1], splitted[0]);
+    graphAverageUserScores(splitted[1], splitted[0]);
   });
 
-  function getScoreAndGraph(title, artist) {
+  const graphUserScores = (title, artist) => {
     $.ajax({
       url: 'showScore',
       type: 'POST',
@@ -116,7 +118,35 @@ $(() => {
 
           $('#scores-graph').empty();
           $('#scores-description').html('<p>Showing scores over time for: ' + title + ', by ' + artist + '.</p>');
-          graphScore([xData, yData], 'graph', 'Scores ', 'scatter')
+          graphScore([xData, yData], 'user', 'Your Scores', 'scatter')
+        }
+      }
+    });
+  }
+    
+  const graphAverageUserScores = (title, artist) => {
+    $.ajax({
+      url: 'average/' + title + '/' + artist,
+      type: 'GET',
+      dataType: 'json',
+      success: (data) => {
+        /* band-aid because the server should send an error code, not the client-side handling it */
+        if (!data.scores) {
+          $('#scores-description').empty();
+          $('#scores-graph').html('<p>No scores found for that song.</p>');
+        } else {
+          data = data.scores;
+          let xData = [];
+          let yData = [];
+          
+          for (let i = 0; i < data.length; ++i) {
+            xData.push(data[i].Date);
+            yData.push(data[i].Score);
+          }
+
+          $('#scores-graph').empty();
+          $('#scores-description').html('<p>Showing scores over time for: ' + title + ', by ' + artist + '.</p>');
+          graphScore([xData, yData], 'avg', 'Average of User Scores', 'scatter')
         }
       }
     });
