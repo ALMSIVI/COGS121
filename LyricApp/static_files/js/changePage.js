835ae4b82811b22c3dfe533228589b57e58f34a9
irenegee <irg004@ucsd.edu>
2018-06-02 13:21:37 -0700

    game css

--- LyricApp/static_files/js/changePage.js
+++ LyricApp/static_files/js/changePage.js
@@ -5,6 +5,13 @@ $(() => {
     if (page === "") page = "add";
     
     $("#content").load(page + ".html");
+
+    if(page === "game"){
+    	document.body.style.background = "black";
+    }
+    else{
+    	document.body.style.background = "white";
+    }
   };
   
   window.onhashchange = changePage;
